c1204ec5ec01d45b86dc65d39240560228b5051f
Artur <am29-10@mail.ru>
2023-03-17 13:35:32 +0300

    Добавить реализацию очистки файла

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
     }
 }
